ce0aa3351f2d95175d3beadc42f6ba8c3de75396
sollpostacaracciolo <jazzcaracciolo@gmail.com>
2025-05-12 17:12:27 -0300

    Delete wdd131 directory

--- wdd131/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-// Footer information
-const currentYear = new Date().getFullYear();
-const lastModified = document.lastModified;
-const copyrightYearElement = document.getElementById('currentyear');
-const lastModifiedElement = document.getElementById('lastModified');
-copyrightYearElement.textContent = currentYear;
-lastModifiedElement.textContent = `Last upd `
